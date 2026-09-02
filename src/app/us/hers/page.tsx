5195678ff831f1226dd9b35c0d03ec78dd24e8e9
tyzingmke <antonymburu379@gmail.com>
2026-09-02 10:39:49 +0300

    Build distinct private member dashboards

---
+++ src/app/us/hers/page.tsx
@@ -0,0 +1,5 @@
+import UniverseApp from "@/components/universe/UniverseApp";
+
+export default function HerPrivatePage() {
+  return <UniverseApp view="home" portal="hers" />;
+}
---
+++ src/app/us/mine/page.tsx
@@ -0,0 +1,5 @@
+import UniverseApp from "@/components/universe/UniverseApp";
+
+export default function MyPrivatePage() {
+  return <UniverseApp view="home" portal="mine" />;
+}
